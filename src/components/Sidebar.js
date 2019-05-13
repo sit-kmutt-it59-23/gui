d228669fbdd5dc8201703d1c788a76694b02312a
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-05-13 15:29:52 +0700

    [UPDATE] import Link in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,23 @@
 import React, {Component} from 'react';
-import {
-    ListGroup,
-    ListGroupItem,
-    } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 export default class Sidebar extends Component {
     render() {
         return (
             <ListGroup>
-                <ListGroupItem disabled tag="a" href="#">เสนอโครงการ</ListGroupItem>
-                <ListGroupItem tag="a" href="#">โครงการที่ผ่านการพิจารณางบ</ListGroupItem>
-                <ListGroupItem tag="a" href="#">กรอกแบบฟอร์มโครงการ</ListGroupItem>
-                <ListGroupItem tag="a" href="#">ติดตามโครงการ</ListGroupItem>
-                <ListGroupItem tag="a" href="#">แบบเสนอโครงการที่อนุมัติ</ListGroupItem>
-                <ListGroupItem disabled tag="a" href="#">สรุปโครงการ</ListGroupItem>
-                <ListGroupItem tag="a" href="#">โครงการที่รอสรุปผล</ListGroupItem>
-                <ListGroupItem tag="a" href="#">โครงการที่กำลังสรุปผล</ListGroupItem>
-                <ListGroupItem tag="a" href="#">โครงการที่สรุปผลแล้ว</ListGroupItem>
-                <ListGroupItem disabled tag="a" href="#">ข้อมูลชมรม</ListGroupItem>
+                <ListGroupItem disabled>เสนอโครงการ</ListGroupItem>
+                <Link to="/projects"><ListGroupItem>โครงการที่ผ่านการพิจารณางบ</ListGroupItem></Link>
+                <Link to="#"><ListGroupItem>กรอกแบบฟอร์มโครงการ</ListGroupItem></Link>
+                <Link to="#"><ListGroupItem>ติดตามโครงการ</ListGroupItem></Link>
+                <Link to="#"><ListGroupItem>แบบเสนอโครงการที่อนุมัติ</ListGroupItem></Link>
+
+                <ListGroupItem disabled>สรุปโครงการ</ListGroupItem>
+                <Link to="#"><ListGroupItem>โครงการที่รอสรุปผล</ListGroupItem></Link>
+                <Link to="#"><ListGroupItem>โครงการที่กำลังสรุปผล</ListGroupItem></Link>
+                <Link to="#"><ListGroupItem>โครงการที่สรุปผลแล้ว</ListGroupItem></Link>
+
+                <ListGroupItem disabled>ข้อมูลชมรม</ListGroupItem>
             </ListGroup>
         );
     } 
