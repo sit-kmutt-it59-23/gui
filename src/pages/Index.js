6196d7a7d18459c316facf3e570e0a05750c1075
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-05-13 15:25:38 +0700

    [UPDATE] edit Index.js => edit layout and import components

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from 'react'
+
 import DefaultLayout from 'layouts/DefaultLayout';
+import ShowBudgetClub from 'components/ShowBudgetClub';
+import ClubName from 'components/ClubName';
 import Sidebar from 'components/Sidebar';
 
-const Index = () => {
-    return (
-        <DefaultLayout sidebar={<Sidebar />}>
-            <p>something</p>
-        </DefaultLayout>
-    );
-};
+const Index = () => (
+    <DefaultLayout
+        left={<Sidebar />}
+        middle={<ClubName />}
+        right={<ShowBudgetClub />}
+    />
+)
 
 export default Index;
