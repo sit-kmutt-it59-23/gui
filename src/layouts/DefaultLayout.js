13aa4d53ad69954cddfac713d09610b73106cb6f
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-05-18 21:48:42 +0700

    [UPDATE] edit container => container-fluid

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -8,7 +8,7 @@ class DefaultLayout extends Component {
         return (
             <Fragment>
                 <Navbar />
-                <Container className="mt-3 mb-3">
+                <Container fluid className="mt-3 mb-3">
                     <Row>
                         <Col sm="12" md="3">{this.props.left}</Col>
                         <Col sm="12" md="6">{this.props.middle}</Col>
