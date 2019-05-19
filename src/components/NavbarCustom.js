3a9a30f222f92bc2b4c126c62af71432671d103d
Phongphan Bunlom <phongphan.phb@mail.kmutt.ac.th>
2019-05-19 13:52:56 +0700

    [UPDATE] Change a project name.

--- src/components/NavbarCustom.js
+++ src/components/NavbarCustom.js
@@ -24,7 +24,7 @@ class NavbarCustom extends Component {
     render() {
         return (
             <Navbar color="primary" dark expand="md">
-                <NavbarBrand href="/">IT59-23</NavbarBrand>
+                <NavbarBrand href="/">KMUTT Actxis</NavbarBrand>
                 <NavbarToggler onClick={this.toggle} />
                 <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
