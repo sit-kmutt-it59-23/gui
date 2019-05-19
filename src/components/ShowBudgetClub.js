42d82de87542eadeba12cd5cdadfb192862538e3
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-05-20 02:48:13 +0700

    [UPDATE] edit component show budget change table => ListGroup

--- src/components/ShowBudgetClub.js
+++ src/components/ShowBudgetClub.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
-import { Table } from 'reactstrap';
+import { ListGroup, ListGroupItem} from 'reactstrap';
+import styled from 'styled-components';
 
+const ListGroupItemCustom = styled(ListGroupItem)`
+    font-size:12px;
+    border:none
+
+    &:frist-child{
+        font-weight:bold;
+    }
+`
 
 class ShowBudgetClub extends Component {
     render() {
         return (
-            <Table responsive>
-                    <thead>
-                        <tr className="text-center">
-                            <th colSpan="3">ปีงบประมาณ ...</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>งบประมาณทั้งหมด</td>
-                            <td>....</td>
-                            <td>บาท</td>
-                        </tr>
-                        <tr>
-                            <td>ใช้ไป</td>
-                            <td>.....</td>
-                            <td>บาท</td>
-                        </tr>
-                        <tr>
-                            <td>คงเหลือ</td>
-                            <td>........</td>
-                            <td>บาท</td>
-                        </tr>
-                    </tbody>
-            </Table>
+            <ListGroup>
+                <ListGroupItemCustom color="dark" className="text-center" >งบประมาณปี 2562</ListGroupItemCustom>
+                <ListGroupItemCustom color="secondary" className="clearfix">
+                    <span className="float-left">ทั้งหมด</span>
+                    <span className="float-right">200,000 บาท</span>
+                </ListGroupItemCustom>
+                <ListGroupItemCustom color="secondary" className="clearfix">
+                    <span className="float-left">ใช้ไป</span>
+                    <span className="float-right">50,000 บาท</span>
+                </ListGroupItemCustom>
+                <ListGroupItemCustom color="secondary" className="clearfix">
+                    <span className="float-left">คงเหลือ</span>
+                    <span className="float-right">150,000 บาท</span>
+                </ListGroupItemCustom>
+                
+          </ListGroup>
         );
     }
 }
