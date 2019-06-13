b211ddd28107ea18de7c18f4f65f010ef1af7e0b
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-06-13 21:21:21 +0700

    [CREATE] Add css to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
+import './assets/css/fonts.css'
+import './assets/css/styles.css'
+import './assets/css/responsive.css'
 
 import createStore from './redux'
 
