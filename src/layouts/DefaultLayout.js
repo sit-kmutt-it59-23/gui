e415e2a11a3fa4ff3f6c1f931667a800f241bc83
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-06-20 18:28:14 +0700

    [DELETE] delete breadcrumb

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -14,43 +14,23 @@ const CustomContentLayout = styled(Layout)`
 
 class DefaultLayout extends Component {
     render() {
-        let breadcrumb;
-        if (this.props.breadcrumb) {
-            breadcrumb = this.props.breadcrumb.map((item, idx) => {
-                return (
-                    <Breadcrumb.Item key={idx}>
-                        {
-                            item.link || !item.active
-                            ? <Link to={item.link}>{item.text}</Link>
-                            : item.text
-                        }
-                    </Breadcrumb.Item>
-                )
-            })
-        }
-        else {
-            breadcrumb = <Breadcrumb.Item><Link to="/">หน้าแรก</Link></Breadcrumb.Item>
-        }
         return (
             <Layout>
                 <Navbar />
                 <CustomContentLayout>
                     <Sidebar />
-                    <Layout style={{ padding: '0 24px 24px' }}>
-                        <Breadcrumb separator=">" style={{ margin: '16px 0' }}>
-                            {breadcrumb}
-                        </Breadcrumb>
-                        <Content
-                            style={{
-                                background: '#fff',
-                                padding: 24,
-                                margin: 0,
-                                minHeight: 280,
-                            }}
-                        >
-                            {this.props.middle}
-                        </Content>
-                    </Layout>
+                        <Layout >
+                            <Content
+                                style={{
+                                    background: '#fff',
+                                    padding: 24,
+                                    margin: 0,
+                                    minHeight: 280,
+                                }}
+                            >
+                                {this.props.middle}
+                            </Content>
+                        </Layout>
                 </CustomContentLayout>
             </Layout>
         )
