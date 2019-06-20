41825a9c9fc363cc269b48388f6591c7762ae2e1
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-06-20 19:02:37 +0700

    [UPDATE] add propType in dafaultLayout

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Layout } from 'antd'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 import Navbar from '../components/NavbarCustom'
 import Sidebar from '../components/Sidebar'
 
-const { Content } = Layout
+const { Content, Sider } = Layout
 
 const CustomContentLayout = styled(Layout)`
     min-height: calc(100vh - 64px) !important;
@@ -29,11 +30,17 @@ class DefaultLayout extends Component {
                             >
                                 {this.props.middle}
                             </Content>
+                            {this.props.right ? <Sider>{this.props.right}</Sider>: null}
                         </Layout>
                 </CustomContentLayout>
             </Layout>
         )
     }
+}
+
+DefaultLayout.propTypes = {
+    middle: PropTypes.element,
+    right: PropTypes.element
 }
 
 export default DefaultLayout
