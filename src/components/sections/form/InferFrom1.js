07c2cb773791583f88b3a3fdf69ea9776e528a6d
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-07-02 21:32:25 +0700

    [UPDATE] change name variable in infer form 1 and delete Input.Group

--- src/components/sections/form/InferFrom1.js
+++ src/components/sections/form/InferFrom1.js
@@ -3,7 +3,7 @@ import { Form, Input, InputNumber, Col } from 'antd'
 import styled from 'styled-components'
 
 const FormItem = Form.Item
-const { TextArea, Group } = Input
+const { TextArea } = Input
 
 const SpanMarginLeft = styled.span`
     padding-left: 20px;
@@ -12,7 +12,7 @@ const SpanMarginLeft = styled.span`
 class InferFrom extends Component {
     render() {
         const { getFieldDecorator } = this.props.form
-        const NameLayout = {
+        const nameLayout = {
             labelAlign: 'left',
             labelCol: {
                 xs: { span: 24 },
@@ -27,7 +27,7 @@ class InferFrom extends Component {
             }
         }
 
-        const NameEnLayout = {
+        const nameEnLayout = {
             labelAlign: 'left',
             labelCol: {
                 xs: { span: 24 },
@@ -42,17 +42,15 @@ class InferFrom extends Component {
             }
         }
 
-        const TopicLayout = {
+        const topicLayout = {
             labelAlign: 'left',
             labelCol: {
-                xs: { span: 24 },
-                sm: { span: 24 },
+                sm: { span: 20, offset: 2 },
                 md: { span: 21, offset: 3 },
-
             }
         }
 
-        const NumberLayout = {
+        const numberLayout = {
             labelAlign: 'left',
             labelCol: {
                 xs: { span: 24, offset: 0 },
@@ -68,7 +66,7 @@ class InferFrom extends Component {
             colon: false
         }
 
-        const TextAreaLayout = {
+        const textAreaLayout = {
             labelAlign: 'left',
             labelCol: {
                 xs: { span: 24, offset: 0 },
@@ -83,7 +81,7 @@ class InferFrom extends Component {
             colon: false
         }
 
-        const ListTextField = {
+        const listTextField = {
             labelCol: {
                 xs: { span: 2, offset: 0 },
                 sm: { span: 1, offset: 2 },
@@ -97,7 +95,7 @@ class InferFrom extends Component {
             colon: false
         }
 
-        const CountLayout = {
+        const countLayout = {
             labelAlign: 'left',
             labelCol: {
                 xs: { offset: 4 },
@@ -105,129 +103,125 @@ class InferFrom extends Component {
                 md: { span: 9, offset: 2 },
             },
             wrapperCol: {
-                xs: { offset: 4 },
+                xs: { offset: 1 },
             },
             colon: false
         }
 
         return (
             <Form >
-                <FormItem label="ชื่อโครงการ" {...NameLayout}>
+                <FormItem label="ชื่อโครงการ" {...nameLayout}>
                     {getFieldDecorator('name', {})(<Input />)}
                 </FormItem>
-                <FormItem label="ชื่อโครงการ (ภาษาอังกฤษ)" {...NameEnLayout}>
+                <FormItem label="ชื่อโครงการ (ภาษาอังกฤษ)" {...nameEnLayout}>
                     {getFieldDecorator('name_en', {})(<Input />)}
                 </FormItem>
-                <FormItem label="ประเภทโครงการกิจกรรม ได้ดังนี้" {...TopicLayout}>
-                    <FormItem label="1. ด้านพัฒนาทักษะทางวิชาการและวิชาชีพ" {...NumberLayout}>
+                <FormItem label="ประเภทโครงการกิจกรรม ได้ดังนี้" {...topicLayout}>
+                    <FormItem label="1. ด้านพัฒนาทักษะทางวิชาการและวิชาชีพ" {...numberLayout}>
                         {getFieldDecorator('hours_amount-educational')(<InputNumber min={0} />)}
                         <SpanMarginLeft>ชั่วโมง</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="2. ด้านกีฬาและการส่งเสริมสุขภาพ" {...NumberLayout}>
+                    <FormItem label="2. ด้านกีฬาและการส่งเสริมสุขภาพ" {...numberLayout}>
                         {getFieldDecorator('hours_amount-health')(<InputNumber min={0} />)}
                         <SpanMarginLeft>ชั่วโมง</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="3. ด้านกิจกรรมจิตอาสาและบำเพ็ญประโยชน์" {...NumberLayout}>
+                    <FormItem label="3. ด้านกิจกรรมจิตอาสาและบำเพ็ญประโยชน์" {...numberLayout}>
                         {getFieldDecorator('hours_amount-community')(<InputNumber min={0} />)}
                         <SpanMarginLeft>ชั่วโมง</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="4. ด้านทำนุบำรุงศิลปะและวัฒนธรรม" {...NumberLayout}>
+                    <FormItem label="4. ด้านทำนุบำรุงศิลปะและวัฒนธรรม" {...numberLayout}>
                         {getFieldDecorator('hours_amount-arts')(<InputNumber min={0} />)}
                         <SpanMarginLeft>ชั่วโมง</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="5. ด้านพัฒนาคุณลักษณะ" {...NumberLayout}>
+                    <FormItem label="5. ด้านพัฒนาคุณลักษณะ" {...numberLayout}>
                         {getFieldDecorator('hours_amount-characteristics')(<InputNumber min={0} />)}
                         <SpanMarginLeft>ชั่วโมง</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="6. ด้านความภาคภูมิใจและความผูกพันมหาวิทยาลัย" {...NumberLayout}>
+                    <FormItem label="6. ด้านความภาคภูมิใจและความผูกพันมหาวิทยาลัย" {...numberLayout}>
                         {getFieldDecorator('hours_amount-loyality')(<InputNumber min={0} />)}
                         <SpanMarginLeft>ชั่วโมง</SpanMarginLeft>
                     </FormItem>
                 </FormItem>
-                <FormItem label="การเทียบค่ากิจกรรมตามคุณลักษณะที่พึงประสงค์ของมหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี (KMUTT Student QF)" {...TopicLayout}>
-                    <FormItem label="1. ความรู้ (Knowledge)" {...NumberLayout}>
+                <FormItem label="การเทียบค่ากิจกรรมตามคุณลักษณะที่พึงประสงค์ของมหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี (KMUTT Student QF)" {...topicLayout}>
+                    <FormItem label="1. ความรู้ (Knowledge)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-knowledge')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="2. ทักษะเชิงวิชาชีพ (Professional Skill)" {...NumberLayout}>
+                    <FormItem label="2. ทักษะเชิงวิชาชีพ (Professional Skill)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-profession')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="3. ทักษะการคิด (Thinking Skill)" {...NumberLayout}>
+                    <FormItem label="3. ทักษะการคิด (Thinking Skill)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-thinking')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="4. ทักษะการเรียนรู้ (Learning Skill)" {...NumberLayout}>
+                    <FormItem label="4. ทักษะการเรียนรู้ (Learning Skill)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-learning')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="5. ทักษะการสื่อสาร (Communication Skill)" {...NumberLayout}>
+                    <FormItem label="5. ทักษะการสื่อสาร (Communication Skill)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-communication')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="6. ทักษะการจัดการ (Management Skill)" {...NumberLayout}>
+                    <FormItem label="6. ทักษะการจัดการ (Management Skill)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-management')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="7. ภาวะผู้นำ (Leadership)" {...NumberLayout}>
+                    <FormItem label="7. ภาวะผู้นำ (Leadership)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-leadership')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
-                    <FormItem label="8. ความเป็นพลเมือง มจธ. (KMUTT’s citizenship)" {...NumberLayout}>
+                    <FormItem label="8. ความเป็นพลเมือง มจธ. (KMUTT’s citizenship)" {...numberLayout}>
                         {getFieldDecorator('qf_amount-kmutt-citizen')(<InputNumber min={0} max={100} />)}
                         <SpanMarginLeft>%</SpanMarginLeft>
                     </FormItem>
                 </FormItem>
-                <FormItem label="หลักการและเหตุผล (เขียนไม่น้อยกว่าครึ่งกระดาษ A4 และทั้ง3 ย่อหน้าต้องสอดล้องกัน)" {...TopicLayout}>
-                    <FormItem label="ย่อหน้าที่ 1 (อ้างที่มา สาเหตุ ปัญหา)" {...TextAreaLayout}>
+                <FormItem label="หลักการและเหตุผล (เขียนไม่น้อยกว่าครึ่งกระดาษ A4 และทั้ง3 ย่อหน้าต้องสอดล้องกัน)" {...topicLayout}>
+                    <FormItem label="ย่อหน้าที่ 1 (อ้างที่มา สาเหตุ ปัญหา)" {...textAreaLayout}>
                         {getFieldDecorator('rational_criterion_1')(<TextArea rows={5} />)}
                     </FormItem>
-                    <FormItem label="ย่อหน้าที่ 2 (กล่าวถึงสิ่งที่จะทำในโครงการ)" {...TextAreaLayout}>
+                    <FormItem label="ย่อหน้าที่ 2 (กล่าวถึงสิ่งที่จะทำในโครงการ)" {...textAreaLayout}>
                         {getFieldDecorator('rational_criterion_2')(<TextArea rows={5} />)}
                     </FormItem>
-                    <FormItem label="ย่อหน้าที่ 3 (กล่าวถึงประโยชน์ที่ได้รับจากการดำเนินโครงการ)" {...TextAreaLayout}>
+                    <FormItem label="ย่อหน้าที่ 3 (กล่าวถึงประโยชน์ที่ได้รับจากการดำเนินโครงการ)" {...textAreaLayout}>
                         {getFieldDecorator('rational_criterion_3')(<TextArea rows={5} />)}
                     </FormItem>
                 </FormItem>
-                <FormItem label="วัตถุประสงค์ (ไม่เกิน 3 ข้อ)" {...TopicLayout}>
+                <FormItem label="วัตถุประสงค์ (ไม่เกิน 3 ข้อ)" {...topicLayout}>
                     <Col sm={{ span: 20, offset: 2 }} md={{ span: 17, offset: 3 }}>
-                        <Group>
-                            <div className="border-form">
-                                <FormItem label="1." {...ListTextField}>
-                                    {getFieldDecorator('objective_1')(<Input />)}
-                                </FormItem>
-                                <FormItem label="2." {...ListTextField}>
-                                    {getFieldDecorator('objective_2')(<Input />)}
-                                </FormItem>
-                                <FormItem label="3." {...ListTextField}>
-                                    {getFieldDecorator('objective_3')(<Input />)}
-                                </FormItem>
-                            </div>
-                        </Group>
+                        <div className="border-form">
+                            <FormItem label="1." {...listTextField}>
+                                {getFieldDecorator('objective_1')(<Input />)}
+                            </FormItem>
+                            <FormItem label="2." {...listTextField}>
+                                {getFieldDecorator('objective_2')(<Input />)}
+                            </FormItem>
+                            <FormItem label="3." {...listTextField}>
+                                {getFieldDecorator('objective_3')(<Input />)}
+                            </FormItem>
+                        </div>
                     </Col>
                 </FormItem>
-                <FormItem label="ผู้เข้าร่วมโครงการ" {...TopicLayout}>
+                <FormItem label="ผู้เข้าร่วมโครงการ" {...topicLayout}>
                     <Col sm={{ span: 20, offset: 2 }} md={{ span: 17, offset: 3 }}>
-                        <Group>
-                            <div className="border-form">
-                                <FormItem label="1. นักศึกษา" {...CountLayout}>
-                                    {getFieldDecorator('attendee_amount-student')(<InputNumber min={0} />)}
-                                    <SpanMarginLeft>คน</SpanMarginLeft>
-                                </FormItem>
-                                <FormItem label="2. อาจารย์" {...CountLayout}>
-                                    {getFieldDecorator('attendee_amount-lecturer')(<InputNumber min={0} />)}
-                                    <SpanMarginLeft>คน</SpanMarginLeft>
-                                </FormItem>
-                                <FormItem label="3. เจ้าหน้าที่" {...CountLayout}>
-                                    {getFieldDecorator('attendee_amount-personnel')(<InputNumber min={0} />)}
-                                    <SpanMarginLeft>คน</SpanMarginLeft>
-                                </FormItem>
-                                <FormItem label="4. บุคคลในชุมชน/นักเรียน" {...CountLayout}>
-                                    {getFieldDecorator('attendee_amount-outsider')(<InputNumber min={0} />)}
-                                    <SpanMarginLeft>คน</SpanMarginLeft>
-                                </FormItem>
-                            </div>
-                        </Group>
+                        <div className="border-form">
+                            <FormItem label="1. นักศึกษา" {...countLayout}>
+                                {getFieldDecorator('attendee_amount-student')(<InputNumber min={0} />)}
+                                <SpanMarginLeft>คน</SpanMarginLeft>
+                            </FormItem>
+                            <FormItem label="2. อาจารย์" {...countLayout}>
+                                {getFieldDecorator('attendee_amount-lecturer')(<InputNumber min={0} />)}
+                                <SpanMarginLeft>คน</SpanMarginLeft>
+                            </FormItem>
+                            <FormItem label="3. เจ้าหน้าที่" {...countLayout}>
+                                {getFieldDecorator('attendee_amount-personnel')(<InputNumber min={0} />)}
+                                <SpanMarginLeft>คน</SpanMarginLeft>
+                            </FormItem>
+                            <FormItem label="4. บุคคลในชุมชน/นักเรียน" {...countLayout}>
+                                {getFieldDecorator('attendee_amount-outsider')(<InputNumber min={0} />)}
+                                <SpanMarginLeft>คน</SpanMarginLeft>
+                            </FormItem>
+                        </div>
                     </Col>
                 </FormItem>
             </Form>
