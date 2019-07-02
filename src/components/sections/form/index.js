326a532a82bb1e4ad8ce2a67d0d636c376130e3d
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-07-03 00:31:33 +0700

    [UPDATE] rename file InferFrom -> InferForm

--- src/components/sections/form/index.js
+++ src/components/sections/form/index.js
@@ -1,5 +1,5 @@
-import Infer1 from './InferFrom1'
-import Infer2 from './InferFrom2'
-import Infer3 from './InferFrom3'
+import Infer1 from './InferForm1'
+import Infer2 from './InferForm2'
+import Infer3 from './InferForm3'
 
 export { Infer1, Infer2, Infer3 }
