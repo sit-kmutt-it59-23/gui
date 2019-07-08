853724a19bcd44e8a39eba34a3fa4725353affb9
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-07-08 19:31:53 +0700

    [UPDATE] change header fixed imperment to Affix(ant design)

--- src/components/sections/HeaderCustom.js
+++ src/components/sections/HeaderCustom.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Divider } from 'antd'
+import { Divider, Affix } from 'antd'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
@@ -21,68 +21,54 @@ const ChildrenStyle = styled.div`
 `
 
 class HeaderCustom extends Component {
-    constructor(props) {
-        super(props)
-        this.handleScroll = this.handleScroll.bind(this)
-
-        this.state = {
-            fixed: false
-        }
-    }
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll);
-    }
-    handleScroll(event) {
-        if (window.scrollY >= 63) {
-            this.setState({
-                fixed: true
-            })
-        } else {
-            this.setState({
-                fixed: false
-            })
-        }
-    }
     render() {
-        let { topic, description, fixed, headerRight, children, childrenHeight, hideChildenInSize } = this.props
-        let className
-        if (fixed) {
-            className = this.state.fixed ? "header-fixed" : ""
-        }
+        let { topic, description, fixed, headerRight, children, hideChildenInSize } = this.props
+
         return (
             <Fragment>
-                <HeaderStyle className={className}>
-                    <PositionFelx className="header-padding">
-                        <div className="text-left font-size-header">
-                            <div style={{ color: "#464545"}} id="topic">
-                                {topic.length > 41 ? `${topic.slice(0,40)}...` : topic}
+                {fixed ?
+                    <Affix>
+                        <HeaderStyle>
+                            <PositionFelx className="header-padding">
+                                <div className="text-left font-size-header">
+                                    <div style={{ color: "#464545" }} id="topic">
+                                        {topic.length > 41 ? `${topic.slice(0, 40)}...` : topic}
+                                    </div>
+                                    <div style={{ color: "#707070" }} id="description">
+                                        {description}
+                                    </div>
+                                </div>
+                                <div className="hidden-xs hidden-sm">
+                                    {headerRight}
+                                </div>
+                            </PositionFelx>
+                            <Divider style={{ margin: 0 }} />
+                            <ChildrenStyle className={hideChildenInSize}>
+                                {children}
+                            </ChildrenStyle>
+                        </HeaderStyle>
+                    </Affix>
+                :
+                    <HeaderStyle>
+                        <PositionFelx className="header-padding">
+                            <div className="text-left font-size-header">
+                                <div style={{ color: "#464545" }} id="topic">
+                                    {topic.length > 41 ? `${topic.slice(0, 40)}...` : topic}
+                                </div>
+                                <div style={{ color: "#707070" }} id="description">
+                                    {description}
+                                </div>
                             </div>
-                            <div style={{ color: "#707070" }} id="description">
-                                {description}
+                            <div className="hidden-xs hidden-sm">
+                                {headerRight}
                             </div>
-                        </div>
-                        <div className="hidden-xs hidden-sm">
-                            {headerRight}
-                        </div>
-                    </PositionFelx>
-                    <Divider style={{ margin: 0 }} />
-                    <ChildrenStyle className={hideChildenInSize}>
-                        {children}
-                    </ChildrenStyle>
-                </HeaderStyle>
-                {this.state.fixed && fixed ?
-                    <div style={{ height: 90, width: '100%', }}/>
-                    : null
+                        </PositionFelx>
+                        <Divider style={{ margin: 0 }} />
+                        <ChildrenStyle className={hideChildenInSize}>
+                            {children}
+                        </ChildrenStyle>
+                    </HeaderStyle>
                 }
-                {this.state.fixed && fixed ?
-                    <div style={{ height: childrenHeight+10, width: '100%', }} className={hideChildenInSize}/>
-                    : null
-                }
-        
             </Fragment>
         )
     }
@@ -94,13 +80,11 @@ HeaderCustom.propTypes = {
     fixed: PropTypes.bool,
     headerRight: PropTypes.element,
     children: PropTypes.element,
-    childrenHeight: PropTypes.number,
     hideChildenInSize: PropTypes.string
 }
 
 HeaderCustom.defaultProps = {
     fixed: false,
-    childrenHeight: 0
 }
 
 export default HeaderCustom
