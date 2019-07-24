1fd05e91b67301f775dedf9f389dc933db34762d
GuideJunG <chanapa.guidellka@mail.kmutt.ac.th>
2019-07-24 22:33:30 +0700

    [UPDATE] edit defaultlayout component

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,45 +1,37 @@
 import React, { Component } from 'react'
 import { Layout } from 'antd'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 import Navbar from '../components/NavbarCustom'
 import Sidebar from '../components/Sidebar'
 
-const { Content, Sider } = Layout
-
-const CustomContentLayout = styled(Layout)`
-    min-height: calc(100vh - 64px) !important;
-`
+const { Content } = Layout
 
 class DefaultLayout extends Component {
     render() {
         return (
             <Layout>
                 <Navbar />
-                <CustomContentLayout>
-                    <Sidebar />
-                        <Layout >
-                            <Content
-                                style={{
-                                    background: '#fff',
-                                    paddingBottom: 20,
-                                    minHeight: 280,
-                                }}
-                            >
-                                {this.props.middle}
-                            </Content>
-                            {this.props.right ? <Sider>{this.props.right}</Sider>: null}
-                        </Layout>
-                </CustomContentLayout>
+                    <Layout>
+                        <Sidebar />
+                        <Content
+                            style={{
+                                background: '#fff',
+                                paddingBottom: 20,
+                                minHeight: 280,
+                            }}
+                        >
+                            {this.props.middle}
+                        </Content>
+                    </Layout>
             </Layout>
+
         )
     }
 }
 
 DefaultLayout.propTypes = {
     middle: PropTypes.element,
-    right: PropTypes.element
 }
 
 export default DefaultLayout
